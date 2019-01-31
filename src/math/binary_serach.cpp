c9e5a1f4452c8e6db55387e7dbfb0e3d89221e0a
yebityon <thebigmercury@gmail.com>
2019-01-31 12:05:46 +0900

    use template & fix english comment

--- src/math/binary_serach.cpp
+++ src/math/binary_serach.cpp
@@ -1,78 +1,30 @@
-#include <bits/stdc++.h>
-//main(){};
-//
-//
-//
-//@verify 08/10/2018 ------add minimize_bsearch and maximize_bsearch
-auto cal(int mid){
-    //W以下最大の数字を探す
-    return v[mid]<= W;
+template<class T,class U>
+auto cal(int mid, T &v, U &value ){
+    //return the maximum index which v[index] <= value
+    return v[mid].first<=value ;
 }
-auto mcal(int mid){
-    //W以上最小の数字をふ返すふ
-    return v[mid]>=W;
+template<class T,class U>
+auto mcal(int mid,T &v, U &value){
+		//return the minimum index which v[index] >= value
+    return v[mid].first>=value;
 }
-auto  minimize_bsearch(int imin,int imax){ //(imin,imax]
+template<class T,class U>
+auto  minimize_bsearch(int imin,int imax,T &v, U &value){ //(imin,imax]
     while(imax - imin > 1){
         int mid = imin + (imax - imin)/2;
-        if(mcal(mid)) imax = mid;
+        if(mcal(mid,v,value)) imax = mid;
         else imin = mid;
     }
     return imax; //imax is ans
 }
 
-auto  maximize_bsearch(int imin,int imax){ //[imin,imax)
+template<class T,class U>
+auto  maximize_bsearch(int imin,int imax, T &v,U &value){ //[imin,imax)
     while(imax - imin > 1){
         int mid = imin + (imax - imin)/2;
-        if(cal(mid)) imin = mid;
+        if(cal(mid,v,value)) imin = mid;
         else imax = mid;
     }
     return imin; //imin is ans
 }
 
-
-
-// Mot verify yet....
-int UpperBound(int *array,int size,int value){
-    //size := arraysize
-    //value := the value which you want to know index
-    int imax=size-1;//last index
-    int imin=0;
-    while(imax>=imin){
-        int mid=(imax+imin)/2;
-        //UpperBoundは単調増加列の最後の要素をのindexを返すため, iminを詰めていく。
-        if(array[mid]>value)imax=mid-1;
-        else imin=mid+1;
-    }
-    //答えはimaxとなる
-    return imax;
-}
-//lowerboundは単調増加列の最初の要素のindexを返す
-int LowerBound(int *array,int size,int value){
-    int imax=size-1;
-    int imin=0;
-    while(imax>=imin){
-        int mid=(imax+imin)/2;
-        if(array[mid]>=value)imax=mid-1;
-        else imin=mid+1;
-    }
-    return imin;
-}
-void arraybinarysearch(int *array,int size,int value){
-    //両方閉区間
-    /*配列の中から、valueを探索する*/
-    int imax=size-1;
-    int imin=0;
-    int mid;
-    //imaxとiminが重なる場合がある
-    while(imax>=imin){
-        mid=(imax+imin)/2;
-        //次の区間に更新する時、midを見る必要は無い。したがってmid+,-1に区間を変更する。
-        if(array[mid]>value)imax=mid-1;
-        else if(array[mid]<value)imin=mid+1;
-        else {
-            puts("found");
-            //return imin;
-        }
-    }
-}
