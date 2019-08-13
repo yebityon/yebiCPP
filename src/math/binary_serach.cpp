bfef283ee489810ad899997c0010074e40e98d87
yebityon <thebigmercury@gmail.com>
2019-08-13 09:55:36 +0900

    fix comment

--- src/math/binary_serach.cpp
+++ src/math/binary_serach.cpp
@@ -7,30 +7,32 @@
 // AtC  https://atcoder.jp/contests/code-festival-2018-final-open/submissions/4131587
 
 
+/*
+ (imin,imax] -> imax
+ return minimum index such that v[index] >= value
+ auto f(int mid) {return v[mid] >= value;}
+ */
 template<class F>
-auto  minimize_bsearch(int imin,int imax,F &f){ //(imin,imax]
-    /*
-     return minimum index such that v[index] >= value
-     auto f(int mid) {return v[mid] >= value;}
-     */
+auto  minimize(int imin,int imax,F &f){
     while(imax - imin > 1){
         int mid = imin + (imax - imin)/2;
         if(f(mid)) imax = mid;
         else imin = mid;
     }
-    return imax; //imax is ans
+    return imax;
 }
 
+/*
+ [imin,imax) -> imin
+ return maximum index such that v[index] <= value
+ auto f(int mid) {return v[mid] <= value;}
+ */
 template<class F>
-auto  maximize_bsearch(int imin,int imax,F &f){ //[imin,imax)
-    /*
-     return maximum index such that v[index] <= value
-     auto f(int mid) {return v[mid] <= value;}
-     */
+auto  maximize(int imin,int imax,F &f){
     while(imax - imin > 1){
         int mid = imin + (imax - imin)/2;
         if(f(mid)) imin = mid;
         else imax = mid;
     }
-    return imin; //imin is ans
+    return imin;
 }
