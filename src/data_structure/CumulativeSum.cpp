2a7df5b133e4a3d2e73b4540a2f29a52b1365c9a
yebityon <thebigmercury@gmail.com>
2019-09-23 12:08:48 +0900

    using Function template

--- src/data_structure/CumulativeSum.cpp
+++ src/data_structure/CumulativeSum.cpp
@@ -1,25 +1,22 @@
 //#includ <bits/stdc++.h>
 //main(){}
-//verifyed  @https://codeforces.com/contest/1132/submission/50930638
-
-
-template <class T>
+//verifyed  @https://codeforces.com/contest/1132/submission/61095997
+template<class T, class Func = function<T(T,T)>>
 class CumulativeSum{
 public:
     vector<T>sum;
     T e;
-    
-    CumulativeSum(vector<T>v,T e) : sum(v.size()),e(e){
+    CumulativeSum(const vector<T>v,T e,const Func& f = [](T a, T b){return a + b;}) : sum(v.size()),e(e){
         copy(v.begin(),v.end(),sum.begin());
         T past = 0;
-        for(auto && itr : sum){
-            itr += past;
+        for(auto & itr : sum){
+            itr = f(itr,past);
             past = itr;
         }
     }
     //[l,r)
-    auto query(int l, int r){
-        if(r > 0) return (sum[r - 1] - (l == 0 ? 0 : sum[l - 1]));
+    auto query(int l, int r, const Func& f = [](T a,T b){return a - b;}){
+        if(r > 0) return f(sum[r - 1],(l == 0 ? 0 : sum[l - 1]));
         else return e;
         
     }
