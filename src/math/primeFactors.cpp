abf1feb353afd55fe05c73688736e5edc309d642
yebityon <thebigmercury@gmail.com>
2019-09-28 23:54:52 +0900

    rename factolize, which is factolize num, add primefactors which count up number of Prime nunbers

--- src/math/primeFactors.cpp
+++ src/math/primeFactors.cpp
@@ -1,6 +1,16 @@
-vector<int> primeFactors(int n) {
-    vector<int> v;
-    int i = 2;
+// @verifyed : https://atcoder.jp/contests/abc142/submissions/7772922
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ Factorize the num.
+ 60 -> {2,2,3.5}
+ 2*2*3*5 = 60;
+ 1 -> 0;
+ */
+template<typename T>
+vector<T> factorize(T n) {
+    vector<T> v;
+    T i = 2;
     while (i * i <= n) {
         if (n % i == 0) {
             v.push_back(i);
@@ -11,4 +21,16 @@ vector<int> primeFactors(int n) {
     }
     if(n != 1)v.push_back(n);
     return v;
+};
+template<typename T>
+set<T> primeFactors( const vector<T>& vec){
+    /* vecotor must be factolized*/
+    set<T> res;
+    for(auto e : vec){
+        res.insert(e);
+    }
+    return res;
 }
+int main(){
+}
+
