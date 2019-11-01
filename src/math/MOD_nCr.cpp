863f1681171e5be2e2490f39a98294f479bd1006
yebityon <thebigmercury@gmail.com>
2019-11-01 15:11:12 +0900

    fix mod

--- src/math/MOD_nCr.cpp
+++ src/math/MOD_nCr.cpp
@@ -1,18 +1,21 @@
-constexpr int MOD = 1e9 + 7;
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int mod = 1e9 + 7;
 template <class T> T modpow(T a, T b) {
     if (b == 0) return T(1);
     if (b % 2 == 0) {
         long long d = modpow(a, b / T(2));
-        return (d * d) % MOD;
+        return (d * d) % mod;
     } else {
-        return (a * modpow(a, b - 1) % MOD);
+        return (a * modpow(a, b - 1) % mod );
     }
 }
-template<class T> T mod_nCr(T n, T r, T MOD) {
+template<class T> T mod_nCr(T n, T r, T mod ) {
     T denom = 1;
     T numer = 1;
-    for (ll i = 1; i <= r; i++) numer = (numer * i) % MOD;
-    for (ll i = n - r + 1; i <= n; i++) denom = (denom * i) % MOD;
-    return (denom * modpow(numer, MOD - 2) % MOD);
+    for (T i = 1; i <= r; i++) numer = (numer * i) % mod;
+    for (T i = n - r + 1; i <= n; i++) denom = (denom * i) % mod;
+    return (denom * modpow(numer, mod - 2) % mod);
 }
-signed main() { cout << mod_nCr(10, 2, MOD) << endl; }
+signed main() { cout << mod_nCr(10, 2, mod) << endl; }
