2e8ac936e4ef6615ef4937ded9f891adaa430fef
yebityon <thebigmercury@gmail.com>
2019-11-10 18:38:19 +0900

    test

---
+++ CITest/union_find_CI.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A
+#include "src/data_structure/union-find.hpp"
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, q; cin >> n >> q;
+    auto uf = unionfind(n);
+    while (q --) {
+        int com, x, y; cin >> com >> x >> y;
+        if (com == 0) {
+            ut.unite(x, y);
+        } else if (com == 1) {
+            cout << ut.same(x, y) << endl;
+        }
+    }
+    return 0;
+}
+© 2019 GitHub, Inc
