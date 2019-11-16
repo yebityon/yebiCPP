4d733c93215906009c89d668716ac6539440dba1
yebityon <thebigmercury@gmail.com>
2019-11-16 18:55:23 +0900

    final fix

--- CITest/CI_data_structure/segtree_CI.test.cpp
+++ CITest/CI_data_structure/segtree_CI.test.cpp
@@ -16,7 +16,8 @@ int main(){
     if(com == 0){
       Int x,y; cin >> x >> y;
       --x;
-      sgt.update(x,y);
+      auto cp = sgt[x] + y:
+      sgt.update(x,cp);
     } else {
       Int x,y; cin >> x >> y;
       --x;
