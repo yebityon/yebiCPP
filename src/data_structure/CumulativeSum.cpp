bec0707cb824b33fa420451aa3f53daf7dda98c2
yebityon <thebigmercury@gmail.com>
2019-12-06 10:44:24 +0900

    edit cumulativesum

--- src/data_structure/CumulativeSum.cpp
+++ src/data_structure/CumulativeSum.cpp
@@ -1,6 +1,6 @@
-//#includ <bits/stdc++.h>
-//main(){}
-//verifyed  @https://codeforces.com/contest/1132/submission/61095997
+#includ <bits/stdc++.h>
+using namespace std;
+
 template<class T, class Func = function<T(T,T)>>
 class CumulativeSum{
 public:
@@ -16,8 +16,17 @@ public:
     }
     //[l,r)
     auto query(int l, int r, const Func& f = [](T a,T b){return a - b;}){
-        if(r > 0) return f(sum[r - 1],(l == 0 ? 0 : sum[l - 1]));
+        if(r > 0) return f(sum[r - 1],(l == 0 ? e : sum[l - 1]));
         else return e;
         
+    }
+    auto begin(){
+        return sum.begin();
+    }
+    auto end(){
+        return sum.end();
+    }
+    auto& operator[] (int idx){
+        return sum[idx];
     }
 };
