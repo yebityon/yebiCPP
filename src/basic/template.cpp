5b12e4a4e0e521e20d694957e04eaf847b88805a
yebityon <thebigmercury@gmail.com>
2019-12-21 11:20:59 +0900

    update template.cpp

--- src/basic/template.cpp
+++ src/basic/template.cpp
@@ -4,33 +4,19 @@ using ll = long long;
 #define int ll
 #define rep(i,n) for(int i=0;i<n;i++)
 #define all(in) in.begin(), in.end()
-#define INF (sizeof(int) == 4 ? (int)1e9:(int)1e18)
-#define EPS 0.0000000001
-template<class T, class S> void cmin(T &a, const S &b) { if (a > b)a = b; }
-template<class T, class S> void cmax(T &a, const S &b) { if (a < b)a = b; }
-template<typename Head, typename Value> auto vectors(const Head &head, const Value &v) { return vector<Value>(head, v); }
-template<typename Head, typename... Tail> auto vectors(Head x, Tail... tail) { auto inner = vectors(tail...); return vector<decltype(inner)>(x, inner); }
+constexpr int INF = (sizeof(int) == 4 ? (int)1e9:(int)1e18);
+constexpr long double eps = 0.0000000001;
+constexpr int mod = (1e9 + 7);
 using ld  = long double;
 using pii = pair<int,int>;
 using piii = pair<int,pii>;
 int W,H;
-int dx[]={0,0,1,-1}, dy[]={1,-1,0,0};
+const int dx[]={0,0,1,-1}, dy[]={1,-1,0,0};
 bool valid(int x,int y){return (0<=x&&x<W)&&(0<=y&&y<H);}
-void YN(bool f){ cout << (f ? "YES" : "NO");}
-void yn(bool f){ cout << (f ? "yes" : "no");}
-void YeNo(bool f) {cout << (f ? "Yes" : "No");}
+
+template<class T, class S> void cmin(T &a, const S &b) { if (a > b)a = b; }
+template<class T, class S> void cmax(T &a, const S &b) { if (a < b)a = b; }
+template<typename Head, typename Value> auto vectors(const Head &head, const Value &v) { return vector<Value>(head, v); }
+template<typename Head, typename... Tail> auto vectors(Head x, Tail... tail) { auto inner = vectors(tail...); return vector<decltype(inner)>(x, inner); }
 signed main(){
-    int n; cin >> n;
-    vector<int>p(n);
-    vector<int>v(150002,0LL);
-    for(auto e : p) v[e]++;
-    for(int i = 1; i < v.size(); ++i){
-        if(v[i] == 0)continue;
-        if(i != 1 and v[i - 1] == 0){
-            v[i - 1]++; v[i]--;
-        }
-        if(i + 1 != v.size() and v[i] > )
-            }
-    cout << accumulate(all(v),0LL,[](int a, int b){return a += ( b > 0);})
-    << endl;
 }
