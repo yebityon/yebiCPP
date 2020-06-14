f364b08f49caf36cad8d82ba4a6caa31f3297167
yebityon <thebigmercury@gmail.com>
2020-06-14 11:42:14 +0900

    minor update

--- src/basic/template.cpp
+++ src/basic/template.cpp
@@ -10,12 +10,11 @@ template<class T, class S> void cmin(T &a, const S &b) { if (a > b)a = b; }
 template<class T, class S> void cmax(T &a, const S &b) { if (a < b)a = b; }
 template<typename Head, typename Value> auto vectors(const Head &head, const Value &v) { return vector<Value>(head, v); }
 template<typename Head, typename... Tail> auto vectors(Head x, Tail... tail) { auto inner = vectors(tail...); return vector<decltype(inner)>(x, inner); }
-using ld  = long double;
-using pii = pair<int,int>;
-using piii = pair<int,pii>;
+using ld  = long double;using pii = pair<int,int>;using piii = pair<int,pii>;
 int W,H;
 int dx[]={0,0,1,-1}, dy[]={1,-1,0,0};
 bool valid(int x,int y){return (0<=x&&x<W)&&(0<=y&&y<H);}
+void fast_io(){ios::sync_with_stdio(false); cin.tie(nullptr);}
 signed main(){
 }
 
--- src/math/Eratosthenes.cpp
+++ src/math/Eratosthenes.cpp
@@ -1,17 +1,17 @@
 class Prime{
+    public :
     vector<bool>Isprime;
     vector<int>prime;
-public:
     int size;
-    Prime(int n) : Isprime(n+1,true),size(n+1){
+    Prime(int N) : Isprime(N+1,true),size(N+1){
         Isprime[0] = Isprime[1] = false;
-        for(int i = 2; i<=size; i++){
+        for(int i = 2; i<= size; i++){
             if(!Isprime[i])continue;
             prime.push_back(i);
             for(int j = 2;i*j <=size;j++) Isprime[i*j] = false;
         }
     }
-    bool IsPrime(int n){return (n<=size? Isprime[n]:false);}
+    bool IsPrime(int N){return ( N <= size? Isprime[N]:false);}
     int indexOf(int i){return (i<prime.size()?prime[i]:INF);}
 };
 
