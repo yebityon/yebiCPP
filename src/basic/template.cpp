d3dff131a74c46244893044a2eb710aba0653f5e
yebityon <thebigmercury@gmail.com>
2020-07-03 22:35:10 +0900

    add template

--- src/basic/template.cpp
+++ src/basic/template.cpp
@@ -6,6 +6,7 @@ using ll = long long;
 #define all(in) in.begin(), in.end()
 constexpr int INF = (numeric_limits<int>::max() == INT_MAX ? (int)1e9 : (long long)1e18);
 constexpr long double eps = 1e-6;
+const string el = "\n";
 template<class T, class S> void cmin(T &a, const S &b) { if (a > b)a = b; }
 template<class T, class S> void cmax(T &a, const S &b) { if (a < b)a = b; }
 template<typename Head, typename Value> auto vectors(const Head &head, const Value &v) { return vector<Value>(head, v); }
@@ -15,6 +16,19 @@ int W,H;
 int dx[]={0,0,1,-1}, dy[]={1,-1,0,0};
 bool valid(int x,int y){return (0<=x&&x<W)&&(0<=y&&y<H);}
 void fast_io(){ios::sync_with_stdio(false); cin.tie(nullptr);}
-signed main(){
+int out(int a){cout << a << el; return 0;}
+int out(string s = "NO"){cout << s << el; return 0;}
+template <class T>
+inline int out(vector<T>&v,string c = " "){
+    for(int i = 0; i < v.size(); ++i){
+        if(i) cout << c; cout << v[i];
+    } cout << el;return 0;
 }
-
+inline void solve(){
+    return;
+}
+signed main(){
+    fast_io();
+    int Q = 1; cin >> Q;
+    while(Q--){solve();}
+}
