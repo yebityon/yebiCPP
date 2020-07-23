5d7c35943fb0a9b65cbd2b736ed3d39240af2e49
yebityon <thebigmercury@gmail.com>
2020-07-24 02:28:55 +0900

    add SCC CI test

---
+++ CITest/CI_data_structure/SCC.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM http://http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_3_C&lang=jp
+#include<bits/stdc++.h>
+#include "src/graph/SCC.cpp"
+using namespace std;
+using Int = long long;
+int main(){
+	  std::ios::sync_with_stdio(false);
+		cin.tie(nullptr);
+		cout.tie(nullptr);
+    int N,E;
+    cin >> N >> E;
+    vector<vector<int>>v(N);
+    for(int i = 0; i < E; ++i){
+        int s,t; cin >> s >> t;
+        v[s].push_back(t);
+    }
+    StronglyConnectedComponents<int>scc(v);
+    scc.build();
+    int Q;
+    cin >> Q;
+    while(Q--){
+        int a,b; cin >> a >> b;
+        cout  << (scc.isSamePointSet(a,b) ? "1" : "0") << endl;
+    }
+}
