3079554328ae2fcbedf45e12da85716ead65fb41
Taeyong Seong <Taeyong Seong>
2020-11-01 16:17:22 +0900

    add CITest

---
+++ CITest/CI_graph/CI_art_point_test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_3_A&lang=jp
+#include<bits/stdc++.h>
+using namespace std;
+#include "../../src/graph/art_point.cpp"
+
+signed main(){
+    int m;
+    cin >> N >> m;
+		vector<vector<int>>edge;
+    edge.resize(N);
+    for(int i=0;i<m;i++){
+        int s,t;
+        cin>>s>>t;
+        edge[s].push_back(t);
+        edge[t].push_back(s);
+    }
+		auto ap = art_point(N,edge);
+    art_point();
+		ap.show_art_point();
+}
