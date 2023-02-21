ddb45638669891a3372a8fb8cf8f9d3c8c31eb79
yebityon <thebigmercury@gmail.com>
2023-02-21 23:17:16 +0900

    update

--- src/math/divisors.hpp
+++ src/math/divisors.hpp
@@ -2,21 +2,20 @@
 // https://atcoder.jp/contests/abc142/submissions/7772465
 #include <bits/stdc++.h>
 using namespace std;
+
+// input : 6 ->  1 2 3 6
+// input : 1 -> 1
+// input : 2 -> 2
 template<typename T>
 auto divisors(T n){
     vector<T>v;
     for(T i = 1; i *  i <= n;i++){
         if(n % i)continue;
-        v.push_back(i);
+        v.emplace_back(i);
         if(i != 1 and i * i != n)
-            v.push_back(n /i);
+            v.emplace_back(n /i);
     }
-    // input : 6 ->  1 2 3 6
-    // input : 1 -> 1
-    // input : 2 -> 2
-    if(n != 1) v.push_back(n);
+    if(n != 1) v.emplace_back(n);
     sort(v.begin(),v.end());
     return v;
 }
-
-
