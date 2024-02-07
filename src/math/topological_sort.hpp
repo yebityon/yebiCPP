4788dcd00f74594e22f4fce051bec42879dd50b6
T.S <thebigmercury@gmail.com>
2024-02-07 23:14:12 +0900

    reaname math/*cpp -> math/*.hpp

---
+++ src/math/topological_sort.hpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+auto TopologicalSort(vector<vector<int>> &edge) {
+    int N = edge.size();
+    vector<int> v(N, false);
+    vector<int> out(N);
+    int idx                 = N - 1;
+    function<void(int)> dfs = [&](int u) -> void {
+        v[u] = true;
+        for (int i = 0; i < edge[u].size(); i++) {
+            int nv = edge[u][i];
+            if (!v[nv]) dfs(nv);
+        }
+        out[idx--] = u;
+    };
+    for (int i = 0; i < v.size(); i++) {
+        if (!v[i]) dfs(i);
+    }
+    return out;
+}
