d766019bc566e8ba8e0f744e2764ec29da2b4561
T.S <thebigmercury@gmail.com>
2024-02-15 23:32:58 +0900

    tempalte追加 + コメント追加

--- src/math/mod_comb.hpp
+++ src/math/mod_comb.hpp
@@ -1,21 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template<class T>
 class modcomb {
 public:
-    int maxn,MOD;
-    vector<int>fac,finv,inv;
-    modcomb(int _maxn,int _mod) : maxn(_maxn),MOD(_mod),fac(_maxn),finv(_maxn),inv(_maxn){
+
+    modcompb(T _maxn, T _mod):maxn(_maxn),mod(_mod),fac(_maxn),finv(_maxn),inv(_maxn){
         fac[0] = fac[1] = 1;
         finv[0] = finv[1] = 1;
         inv[1] = 1;
-        for (int i = 2; i < maxn; i++){
-            fac[i] = fac[i - 1] * i % MOD;
-            inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
-            finv[i] = finv[i - 1] * inv[i] % MOD;
+        for (T i = T(2); i < maxn; i++){
+            // iの階乗の逆元
+            fac[i] = fac[i - 1] * i % mod;
+            // iの逆元
+            inv[i] = mod - inv[mod%i] * (mod / i) % mod;
+            // iの階乗の逆元
+            finv[i] = finv[i - 1] * inv[i] % mod;
         }
     };
-    int modComb(int n,int k){
-        if (n < k) return 0LL;
-        if (n < 0 || k < 0) return 0LL;
-        return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
-    }
     
+    T maxn,mod;
+    vector<T>fac,finv,inv;
+
+    // calcuration of nCk with mod
+    T modComb(int n,int k){
+        if (n < k) return T(0);
+        if (n < 0 || k < 0) return T(0);
+        return fac[n] * (finv[k] * finv[n - k] % mod) % mod;
+    }
 };
